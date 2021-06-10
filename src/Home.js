ca3df1974a780c74f99ed7dc66650d657ad95b32
timllanto <timkristianllanto.tk@gmail.com>
2021-06-10 17:08:27 +0800

    Fixed CourseDescrip navigation

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,6 @@
 import { Route, Switch } from "react-router";
 import Layout from "./components/Layout";
 import useToggle from "./hooks/useToggle";
-import CourseDescrip1 from "./pages/CourseDescrip1";
-import CourseDescrip2 from "./pages/CourseDescrip2";
 import MainPage1 from "./pages/MainPage1";
 import MainPage2 from "./pages/MainPage2";
 import SettingsPage from "./pages/SettingsPage";
@@ -35,13 +33,6 @@ const Home = ({ hasIntroData }) => {
             <Switch>
               <Route path="/main2">
                 <MainPage2 editMode={editMode} />
-              </Route>
-
-              <Route path="/coursedescrip1">
-                <CourseDescrip1 />
-              </Route>
-              <Route path="/coursedescrip2">
-                <CourseDescrip2 />
               </Route>
               <Route path="/">
                 <MainPage1 editMode={editMode} />
